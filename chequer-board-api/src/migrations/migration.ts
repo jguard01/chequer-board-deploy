91a9a76949dc1207277eb4e45101b1119882c9c9
jguard01 <gyeongho7551@gmail.com>
2021-09-26 07:56:25 +0900

    dockerization done

--- chequer-board-api/src/migrations/migration.ts
+++ chequer-board-api/src/migrations/migration.ts
@@ -1,9 +1,23 @@
 import type { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class CreatePostsTable1622299665807 implements MigrationInterface {
-    name = 'createPostsTable1622299665807';
-
+export class Migration1622299665806 implements MigrationInterface {
+    name = 'Migration1622299665806';
     public async up(queryRunner: QueryRunner): Promise<void> {
+
+        await queryRunner.query(`CREATE TABLE users
+                                 (
+                                     id        VARCHAR(50),
+                                     created_at TIMESTAMP NOT NULL DEFAULT now(),
+                                     updated_at TIMESTAMP NOT NULL DEFAULT now(),
+                                     fullname  VARCHAR(100),
+                                     username  VARCHAR(100),
+                                     email      VARCHAR(100),
+                                     password   VARCHAR(100),
+                                     phone      VARCHAR(100),
+                                     CONSTRAINT UC_username UNIQUE (username),
+                                     PRIMARY KEY (id)
+                                 )`);
+
         await queryRunner.query(`CREATE TABLE posts
                                  (
                                      post_id   INT AUTO_INCREMENT primary key NOT NULL,
@@ -19,7 +33,9 @@ export class CreatePostsTable1622299665807 implements MigrationInterface {
                                  )`);
     }
 
+
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query('DROP TABLE users');
         await queryRunner.query('DROP TABLE posts');
     }
 }
